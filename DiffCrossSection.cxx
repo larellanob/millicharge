f128ec207bdd5dabacb6f5adec1b7bfb4254df3a
Luciano Arellano <luciano.gabriel.arellano.bert@cern.ch>
2021-03-21 14:06:31 +0000

    fixed massive slowdown issue caused by calling TDatabasePDG and comparing masses every time

--- DiffCrossSection.cxx
+++ DiffCrossSection.cxx
@@ -4,9 +4,9 @@
 Double_t DiffCrossSection(TLorentzVector mcp,
 			  TLorentzVector mcpbar,
 			  Double_t epsilon,
+			  TString meson,
 			  Double_t mesonmass)
 {
-  TDatabasePDG pdgDiffCrossSection;
   Double_t alpha = 1./137.;
   TLorentzVector g = (mcp)+(mcpbar); // virtual photon resulting in mcp pair
   Double_t s = g.M2();
@@ -27,21 +27,19 @@ Double_t DiffCrossSection(TLorentzVector mcp,
   mcp_in_grest.Boost(-boo); // mcp in photon rest frame
   //mcp_in_grest.Print();
   Double_t theta = mcp_in_grest.Angle(boo);
-  
-  
 
   Double_t factor1 = (epsilon*epsilon*alpha)/(4.0*TMath::Pi()*s);
   Double_t factor2 = pow(1.0 - (s/(mesonmass*mesonmass)),3);
   Double_t factor3 = sqrt(1.0 - (4.0*mcp.M()*mcp.M())/s);
   Double_t factor4 = 2.0 - (1.0 - (4.0*mcp.M()*mcp.M())/s)*pow(sin(theta),2);
   Double_t factor5;
-
-
-  if ( mesonmass == pdgDiffCrossSection.GetParticle(111)->Mass() ) {
+  if ( meson == "pi0" ) {
     factor5 = 0.98823;
-  } else if ( mesonmass == pdgDiffCrossSection.GetParticle(221)->Mass() ) {
+  } else if ( meson == "eta" ) {
     factor5 = 0.3941;
   }
+
+
   return factor1*factor2*factor3*factor4*factor5;
   
 }
