65c7dc7f18e06d0238d0fab2a3b7f895210501a8
Luciano Arellano <luciano.arellano@postgrad.manchester.ac.uk>
2021-11-10 14:22:28 +0000

    makes (thres)x(events)x(scatters) plots from previously generated histograms

--- PlotPublishedLimits.cxx
+++ PlotPublishedLimits.cxx
@@ -1,39 +1,46 @@
 #include "Root/CreateEmptyDirs.cxx"
+void MultiDraw(std::vector<TGraph * >, std::vector<std::vector<std::vector<TGraph *>>>,TH2F*);
 
-Double_t nbkghits(Double_t empty, Int_t hits, double frames)
-{
-  // usage example
-  //std::cout << nbkghits(0.88,1,3.26e6) << std::endl;
-  //return 0;
+// this macro iterates over these three vectors
+
+// comment values you are not interested in
+
+// when trying to add new values you also have to add it to the
+// previous run_Limits.sh list
+
+std::vector<TString> Ethres_vector = {
+  "0.1",
+  "0.6",
+  "0.8",
+  "1.0"
+};
+
+std::vector<int> events = 
+  {
+    1,
+    10,
+    100
+  };
+
+std::vector<int> nhits_vector = {
+  1,
+  2,
+  3,
+  4
+};
   
-  // empty = percentage of empty frames
-  if ( empty > 1 ) {
-    std::cout << "ERROR: can't have that many empty frames " << std::endl;
-    return 0;
-  }
-  double phit = -log(empty);
-  double sum = 0;
-  double oldsum = -1;
-  int n = hits;
-
-  // summing, convergence for 0.00001
-  while ( abs(oldsum-sum) > 0.00001 ) {
-    std::cout << "sum " << sum << std::endl;
-    oldsum = sum;
-    sum += TMath::Binomial(n,hits)*pow(phit,n)/TMath::Factorial(n);
-    n++;
-  }
-  return frames * exp(-phit) * sum;
-}
 
 
 void PlotPublishedLimits()
 {
   CreateEmptyDirs();
-  const char* dir = "/home/luciano/Physics/neutrino/millicharge/data/limits_argoneut_experiment_12hits/";
+
+  // this method of getting the data might create some problems. the
+  // order of file iteration chances (it's not alphabetical), so in
+  // the final plot the legends might be off
+  const char* dir = "/home/luciano/Physics/manchester/millicharge/data/limits_argoneut_experiment_stfcposter/";
   
   //char *dir  = gSystem->ExpandPathName(dir_tstring);
-
   
   void *dirpointer = gSystem->OpenDirectory(dir);
 
@@ -96,31 +103,14 @@ void PlotPublishedLimits()
       tgraphs[exp]->SetPoint(point, experimentsx[exp][point], experimentsy[exp][point]);
     }
   }
+
   
-  //TGraph *gr = new TGraph(e0_size,x,y);
-  TFile *f = new TFile("hist/Lim_argoneut.root");
-  auto c1 = new TCanvas();
-  c1->SetLogy();
-  c1->SetLogx();
-  c1->SetLogz();
-  //tgraphs[1]->Draw("A L");
+  TH2F * limnhit;
 
-  TH2F * lim1hit;
-  lim1hit = (TH2F*)gDirectory->Get("lim1hit");
+  // 3d vector
+  // events_graphs[low energy threshold][n of events][n of scatters]
+  std::vector<std::vector<std::vector<TGraph *>>> events_graphs; 
 
-  TH2F * lim2hit;
-  lim2hit = (TH2F*)gDirectory->Get("lim2hit");
-
-  //nbkghits(Double_t empty, Int_t hits, double frames)
-  Double_t percentage_empty = 0.88;
-  Double_t frames = 3.26e6;
-  Double_t threshold = nbkghits(percentage_empty,1,frames);
-  Double_t threshold2hit = nbkghits(percentage_empty,2,frames);
-
-  std::cout << "threshold " << threshold << std::endl;
-  std::cout << "threshold 2 hit " << threshold2hit << std::endl;
-  TGraph *uboone1hit = new TGraph(8);
-  TGraph *uboone2hit = new TGraph(8);
 
   // x-bin numbers where calculated mass points are stored
   // 0.01 0.02 0.03 0.05 0.06 0.1 0.2 0.25 GeV
@@ -136,106 +126,267 @@ void PlotPublishedLimits()
      12
     };
 
-  // argoneut numbers
-  Double_t resolution_factor = (0.3/470)*(5.6/400); // put microboone resolution
-  
-  std::cout << "Resolution factor " << resolution_factor << std::endl;
-  std::cout << "2-hit bkg (eq. 17) " << threshold2hit << std::endl;
-  std::cout << "2-hit bkg considering alignment (eq. 18) " << resolution_factor*threshold2hit << std::endl;
   int counter = -1;
   std::cout << "ok " << std::endl;
-  for ( int x: massbins ) {
-    counter++;
-    double centerx = lim1hit->GetXaxis()->GetBinCenter(x);
-    bool findlim1hit = true;
-    bool findlim2hit = true;
 
-    // searches the first point in th2f which is greater than
-    // threshold and sets the limit there (stops searching)
-    for ( int y = 0; y < 29; y++ ) {
-      // 1 hit
-      if ( lim1hit->GetBinContent(lim1hit->GetBin(x,y+1)) > threshold  && findlim1hit ) {
+  std::vector<std::vector<TGraph*>> vec2;
+  std::vector<TGraph*> vec1;
+  
+  for ( int eth = 0; eth < Ethres_vector.size(); eth++ ) {
+    TFile *f = new TFile("hist/Lim_uboone_"+Ethres_vector[eth]+".root");
+    vec2.clear();
+    for ( int e = 0; e < events.size(); e++ ) {
+      vec1.clear();
+      for ( auto nhit: nhits_vector ) {
+	if ( nhit == 1 ) {
+	  limnhit = (TH2F*)gDirectory->Get("lim1hit");
+	} else if ( nhit == 2 ) {
+	  limnhit = (TH2F*)gDirectory->Get("lim2hit");
+	} else if ( nhit == 3 ) {
+	  limnhit = (TH2F*)gDirectory->Get("lim3hit");
+	} else if ( nhit == 4 ) {
+	  limnhit = (TH2F*)gDirectory->Get("lim4hit");
+	}
+	TGraph *uboonenhit = new TGraph(8);
+	for ( int x: massbins ) {
+
+	  counter++;
+	  double centerx = limnhit->GetXaxis()->GetBinCenter(x);
+	  bool findlimnhit = true;
+	  
+	  // searches the first point in th2f which is greater than
+	  // threshold and sets the limit there (stops searching)
+	  for ( int y = 0; y < 29; y++ ) {
+	    // find nhit threshold
+
+	    if ( limnhit->GetBinContent(limnhit->GetBin(x,y+1)) > events[e]  && findlimnhit ) {
+	      
+	      double meany
+		= 0.5*(limnhit->GetYaxis()->GetBinCenter(y+1)
+		       + limnhit->GetYaxis()->GetBinCenter(y));
+	      double lowedge = limnhit->GetYaxis()->GetBinLowEdge(y+1);
+	      uboonenhit->SetPoint(counter,centerx,lowedge);
+	      //uboonenhit->SetPoint(counter,centerx,meany);
+	      findlimnhit = false; // stop searching
+	    }
+	    
+	    // if it's the last point and didn't find limit, set it at the
+	    // highest charge (top of y axis)
+	    if ( y == 28 ) {
+	      double meany
+		= 0.5*(limnhit->GetYaxis()->GetBinCenter(y+1)
+		       + limnhit->GetYaxis()->GetBinCenter(y));
+	      if ( findlimnhit == true ) {
+		// reached the end
+		std::cout << "Reached the end and didn't find charge limit" << std::endl;
+		uboonenhit->SetPoint(counter,centerx,meany);
+	      }
+	    }
+	  }
+	}
+	vec1.push_back(uboonenhit);
 	
-	double meany
-	  = 0.5*(lim1hit->GetYaxis()->GetBinCenter(y+1)
-		 + lim1hit->GetYaxis()->GetBinCenter(y));
-	uboone1hit->SetPoint(counter,centerx,meany);
-	findlim1hit = false; // stop searching
       }
-      // 2 hits
-      //if ( lim2hit->GetBinContent(lim2hit->GetBin(x,y+1))*resolution_factor > 3.5 && findlim2hit) {
-      if ( lim2hit->GetBinContent(lim2hit->GetBin(x,y+1)) > 3.5 && findlim2hit) {
-	double meany
-	  = 0.5*(lim2hit->GetYaxis()->GetBinCenter(y+1)
-		 + lim2hit->GetYaxis()->GetBinCenter(y));
-	uboone2hit->SetPoint(counter,centerx,meany);
-	findlim2hit = false; // stop searching
+      vec2.push_back(vec1);
+    }
+    events_graphs.push_back(vec2);
+  }
+
+  std::cout << "finished reading, about to Draw" << std::endl;
+  MultiDraw(tgraphs, events_graphs, limnhit);
+}
+
+void MultiDraw(std::vector<TGraph * > published, std::vector<std::vector<std::vector<TGraph *>>> limits,TH2F* dummy)
+{
+  // draws all combinations of plotsn
+  
+  // e.g. keeping threshold and number of events fixed, make plot for
+  // different number of scatters
+
+  // three loops in total
+
+  std::cout << "published size: " << published.size() << std::endl;
+  std::cout << "limits size: " << limits.size() << std::endl;
+  std::cout << "limits[0] size: " << limits[0].size() << std::endl;
+  std::cout << "limits[0][0] size: " << limits[0][0].size() << std::endl;
+  
+  gStyle->SetOptStat(0);
+  TString detector = "uboone";
+  TString outname;
+
+  // base histogram (axes and title)
+
+  dummy->GetXaxis()->CenterTitle();
+  dummy->GetYaxis()->SetTitle("Millicharge #epsilon = Q/e");
+  dummy->GetXaxis()->SetTitle("Mass m_{#chi} (MeV)");
+  dummy->GetYaxis()->CenterTitle();
+  dummy->Reset();
+
+  //dummy->SetTitle(Ethres+" MeV threshold");
+
+  // experiment lines
+  published[1]->SetLineColor(kBlue);
+  published[1]->SetLineStyle(2);
+  published[1]->SetLineWidth(5);
+
+  published[0]->SetLineColor(kRed);
+  published[0]->SetLineWidth(3);
+
+  published[2]->SetLineColor(kTeal);
+  published[2]->SetLineWidth(5);
+
+  published[3]->SetLineColor(kGray);
+  published[3]->SetLineWidth(3);
+
+  published[4]->SetLineColor(kViolet);
+  published[4]->SetLineWidth(3);
+
+  published[5]->SetLineColor(kBlack);
+  published[5]->SetLineWidth(3);
+
+  published[0]->SetTitle("MilliQ");
+  published[1]->SetTitle("MicroBooNE (estimate using BNB)");
+  published[2]->SetTitle("ArgoNeuT");
+  published[3]->SetTitle("LHC");
+  published[4]->SetTitle("MiniBooNE");
+  published[5]->SetTitle("LSND");
+
+  gStyle->SetOptStat(0);
+
+
+  std::vector<TGraph*>::iterator it3;
+  std::vector<std::vector<TGraph*>>::iterator it2;
+  std::vector<std::vector<std::vector<TGraph*>>>::iterator it1;
+  
+  std::cout << "loop over thresholds " << std::endl;
+  //////////////////////
+  // loop over thesholds
+  for ( int i2 = 0; i2 < limits[0].size(); i2++ ) {
+    for ( int i3 = 0; i3 < limits[0][0].size(); i3++ ) {
+      auto c1 = new TCanvas();
+      c1->SetLogy();
+      c1->SetLogx();
+      c1->SetLogz();
+      dummy->SetTitle(Form("#muBooNE - %i-hit signal events, %i scatters",events[i2],nhits_vector[i3]));
+      dummy->Draw("text");
+      published[3]->Draw("L");
+      published[0]->Draw("L");
+      //published[1]->Draw("L"); // pheno estimate
+      published[2]->Draw("L");
+      published[4]->Draw("L");
+      published[5]->Draw("L");
+  
+      TLegend *myleg = new TLegend(0.5,0.15,0.9,0.45);
+      myleg->AddEntry(published[0]);
+      // myleg->AddEntry(published[1]); pheno estimate
+      myleg->AddEntry(published[2]);
+      myleg->AddEntry(published[3]);
+      myleg->AddEntry(published[4]);
+      myleg->AddEntry(published[5]);
+
+      for ( int i1 = 0; i1 < limits.size(); i1++ ) {
+	limits[i1][i2][i3]->SetLineColor(kGreen+i1);
+	limits[i1][i2][i3]->SetLineWidth(5);
+	limits[i1][i2][i3]->SetTitle(Form("%s MeV threshold",Ethres_vector[i1].Data()));
+	limits[i1][i2][i3]->SetLineStyle(10-i1);
+	limits[i1][i2][i3]->Draw("L");
+	myleg->AddEntry(limits[i1][i2][i3]);
       }
-      // if it's the last point and didn't find limit, set it at the
-      // highest charge (top of y axis)
-      if ( y == 28 ) {
-	double meany
-	  = 0.5*(lim1hit->GetYaxis()->GetBinCenter(y+1)
-		 + lim1hit->GetYaxis()->GetBinCenter(y));
-	if ( findlim1hit == true ) {
-	  // reached the end
-	  std::cout << "Reached the end and didn't find charge limit" << std::endl;
-	  uboone1hit->SetPoint(counter,centerx,meany);
-	}
-	if ( findlim2hit == true ) {
-	  // reached the end
-	  std::cout << "Reached the end and didn't find charge limit" << std::endl;
-	  uboone2hit->SetPoint(counter,centerx,meany);
-	}
-      }
+
+      outname = Form("img/Limits/Limits_loop-thresholds_%ievents_%ihits.pdf",events[i2],nhits_vector[i3]);
+      myleg->Draw();
+      c1->SaveAs(outname);
     }
     
   }
-  gStyle->SetOptStat(0);
-  lim1hit->SetTitle("1 hit");
-  lim1hit->Draw("colz text");
+
+  std::cout << "loop over nevents" << std::endl;
+  //////////////////////
+  // loop over events
+  for ( int i1 = 0; i1 < limits.size(); i1++ ) {
+    for ( int i3 = 0; i3 < limits[0][0].size(); i3++ ) {
+      auto c1 = new TCanvas();
+      c1->SetLogy();
+      c1->SetLogx();
+      c1->SetLogz();
+      dummy->SetTitle(Form("#muBooNE - %s MeV threshold, %i scatters",Ethres_vector[i1].Data(),nhits_vector[i3]));
+      dummy->Draw("text");
+      published[3]->Draw("L");
+      published[0]->Draw("L");
+      //published[1]->Draw("L"); // pheno estimate
+      published[2]->Draw("L");
+      published[4]->Draw("L");
+      published[5]->Draw("L");
   
-  uboone1hit->SetLineColor(kYellow);
-  uboone1hit->SetLineWidth(5);
+      TLegend *myleg = new TLegend(0.5,0.15,0.9,0.45);
+      myleg->AddEntry(published[0]);
+      // myleg->AddEntry(published[1]); pheno estimate
+      myleg->AddEntry(published[2]);
+      myleg->AddEntry(published[3]);
+      myleg->AddEntry(published[4]);
+      myleg->AddEntry(published[5]);
+      
 
-  uboone2hit->SetLineColor(kGreen);
-  uboone2hit->SetLineWidth(5);
-  
-  tgraphs[1]->SetLineColor(kBlue);
-  tgraphs[1]->SetLineWidth(5);
+      for ( int i2 = 0; i2 < limits[0].size(); i2++ ) {
+	limits[i1][i2][i3]->SetLineColor(kGreen+i2);
+	limits[i1][i2][i3]->SetLineWidth(5);
+	limits[i1][i2][i3]->SetTitle(Form("%i signal events",events[i2]));
+	limits[i1][i2][i3]->SetLineStyle(10-i2);
+	limits[i1][i2][i3]->Draw("L");
+	myleg->AddEntry(limits[i1][i2][i3]);
+      }
 
-  tgraphs[0]->SetLineColor(kRed);
-  tgraphs[0]->SetLineWidth(5);
+      outname = Form("img/Limits/Limits_loop-events_%sthres_%ihits.pdf",Ethres_vector[i1].Data(),nhits_vector[i3]);
+      myleg->Draw();
+      c1->SaveAs(outname);
 
-  tgraphs[1]->Draw("same L");
-  uboone1hit->Draw("same L");
-
-  TLegend *myleg;
-  uboone1hit->SetTitle("ArgoNeuT 1 hit our simulation");
-  tgraphs[1]->SetTitle("ArgoNeuT 1 hit published");
-  myleg = c1->BuildLegend(0.6,0.15,0.9,0.35,"","");
-
-  TString detector = "argoneut";
-  TString outname = Form("img/Limits/Limits_1hit_%s_text.png",detector.Data());
-  c1->SaveAs(outname);
-  lim1hit->Draw("colz");
-  tgraphs[1]->Draw("same L");
-  uboone1hit->Draw("same L");
-  outname = Form("img/Limits/Limits_1hit_%s.png",detector.Data());
-  c1->SaveAs(outname);
+    }
     
-  auto c2 = new TCanvas();
-  c2->SetLogy();
-  c2->SetLogx();
-  c2->SetLogz();
-  gStyle->SetOptStat(0);
-  lim2hit->SetTitle("2 hits");
-  lim2hit->Draw("colz");
-  tgraphs[0]->Draw("same L");
-  uboone2hit->Draw("same L");
-  
+  }
 
-  //TF1 * fun = new TF1("fun",[&](double*x, double *p){ return p[0]*tgraphs[1]->Eval(x[0]); }, 2.5, 2.7, 1);
-  //fun->Draw();
+  std::cout << "loop over nhits" << std::endl;
+  //////////////////////
+  // loop over nhits
+  for ( int i1 = 0; i1 < limits.size(); i1++ ) {
+    for ( int i2 = 0; i2 < limits[0].size(); i2++ ) {    
+      auto c1 = new TCanvas();
+      c1->SetLogy();
+      c1->SetLogx();
+      c1->SetLogz();
+      dummy->SetTitle(Form("#muBooNE %s MeV threshold,  %i-hit signal events",Ethres_vector[i1].Data(),events[i2]));      
+      dummy->Draw("text");
+      published[3]->Draw("L");
+      published[0]->Draw("L");
+      //published[1]->Draw("L"); // pheno estimate
+      published[2]->Draw("L");
+      published[4]->Draw("L");
+      published[5]->Draw("L");
+  
+      TLegend *myleg = new TLegend(0.5,0.15,0.9,0.45);
+      myleg->AddEntry(published[0]);
+      // myleg->AddEntry(published[1]); pheno estimate
+      myleg->AddEntry(published[2]);
+      myleg->AddEntry(published[3]);
+      myleg->AddEntry(published[4]);
+      myleg->AddEntry(published[5]);
+      
+      for ( int i3 = 0; i3 < limits[0][0].size(); i3++ ) {
+	limits[i1][i2][i3]->SetLineColor(kGreen+i3);
+	limits[i1][i2][i3]->SetLineWidth(5);
+	limits[i1][i2][i3]->SetTitle(Form("%i scatters",nhits_vector[i3]));
+	limits[i1][i2][i3]->SetLineStyle(10-i3);
+	limits[i1][i2][i3]->Draw("L");
+	myleg->AddEntry(limits[i1][i2][i3]);
+      }
+      
+      outname = Form("img/Limits/Limits_loop-scatters_%sthres_%ievents.pdf",Ethres_vector[i1].Data(),events[i2]);
+      myleg->Draw();
+      c1->SaveAs(outname);
+    }
+    
+  }
+
+  std::cout << "finished. " << std::endl;
   
 }
+  
